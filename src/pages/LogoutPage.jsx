a929ef3db36ea7ff05eb552f3ab5f9c77cc14931
Robert Perillo <perillo.robert@gmail.com>
2020-10-08 15:15:10 -0400

    update client id

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -4,8 +4,6 @@ import tokenServices from '../utils/tokenServices';
 import { UserContext } from "../contexts/UserContext";
 
 
-// const clientId = '742998896262-126r6u5gq1d0jvm1sun5r4up65sicqo8.apps.googleusercontent.com'
-
 const clientId = `${process.env.REACT_APP_GOOGLE_CLIENT_ID}`
 
 
